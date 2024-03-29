96e11b94864078c39649e7d37883d1c54ca1c79a
xxyeon <jykim9335@gmail.com>
2024-03-30 03:05:49 +0900

    initial commit

---
+++ src/main/java/Iniro/kTrip/KTripApplication.java
@@ -0,0 +1,13 @@
+package Iniro.kTrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KTripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KTripApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Iniro/kTrip/KTripApplicationTests.java
@@ -0,0 +1,13 @@
+package Iniro.kTrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KTripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
