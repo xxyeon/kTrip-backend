8c6e7374175929bb18ed725b79a9bf2aaf69b5fc
박진영 <am030813@naver.com>
2024-05-22 19:26:54 +0900

    FIX: 리뷰 등록 시 mid 값도 추가되도록 수정, 추후 토큰 사용 방식으로 수정 필요

--- src/main/java/Iniro/kTrip/service/ReviewService.java
+++ src/main/java/Iniro/kTrip/service/ReviewService.java
@@ -1,17 +1,18 @@
 package Iniro.kTrip.service;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.util.stream.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.*;
 
+import Iniro.kTrip.domain.ReviewData;
 import Iniro.kTrip.dao.*;
 import Iniro.kTrip.domain.*;
-
+import Iniro.kTrip.dto.*;
 import lombok.*;
-import org.springframework.transaction.annotation.Transactional;
-
 import java.util.List;
 @AllArgsConstructor
 @Service
@@ -19,8 +20,25 @@ public class ReviewService {
     @Autowired
     ReviewRepository reviewRepository;
 
-    public void reviewWrite(Review review){reviewRepository.save(review);}
-    public List<Review> reviewList(int ctypeid, int cid){return reviewRepository.findByCtypeidAndCid(ctypeid, cid);}
-    public void reviewDelete(int review_id){reviewRepository.deleteById(review_id);}
+    public void reviewRegister(Review review){reviewRepository.save(review);}
+    public List<Review> reviewLoad(int ctypeid, int cid){return reviewRepository.findByCtypeidAndCid(ctypeid, cid);}
+    public Review reviewCreate(ReviewData reviewData){
+        Review newReview = new Review();
+        Member member = new Member();
+        member.setMid(reviewData.getMid());
+
+        LocalDateTime localDateTime = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        String formattedDateTime = localDateTime.format(formatter);
+
+        newReview.setMember(member);
+        newReview.setCtypeid(reviewData.getCtypeid());
+        newReview.setCid(reviewData.getCid());
+        newReview.setPoint(reviewData.getPoint());
+        newReview.setContent(reviewData.getContent());
+        newReview.setWritedate(formattedDateTime);
+
+        return newReview;
+    }
 
 }
