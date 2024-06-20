f163cafe596a80419ab5a442e6482cff86f3f6ec
박진영 <am030813@naver.com>
2024-06-21 07:50:54 +0900

    ADD : API 접속 후 받아올 데이터 개체

---
+++ src/main/java/Iniro/kTrip/dto/ResAreaInfo.java
@@ -0,0 +1,13 @@
+package Iniro.kTrip.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class ResAreaInfo{
+    private String code;
+    private String name;
+}
