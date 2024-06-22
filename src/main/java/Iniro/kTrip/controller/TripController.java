5b1c63e8daec0ab18bd1bc45d70e9e5d3ebe2ac3
박진영 <am030813@naver.com>
2024-06-23 05:53:08 +0900

    FIX: recommendTrip 매개변수에 contenttypeid, cat1 추가

--- src/main/java/Iniro/kTrip/controller/TripController.java
+++ src/main/java/Iniro/kTrip/controller/TripController.java
@@ -18,9 +18,13 @@ public class TripController {
     private final TripService tripService;
 
     @GetMapping("/recommend")
-    public ResponseEntity<?> recommendTrip(@RequestParam(name = "areacode", required = false) String areaCode,@RequestParam(name = "sigungucode", required = false) String sigunguCode, @RequestParam(name = "pageno", required = false) String pageNo) throws URISyntaxException {
+    public ResponseEntity<?> recommendTrip(@RequestParam(name = "areacode", required = false) String areaCode,
+                                           @RequestParam(name = "sigungucode", required = false) String sigunguCode,
+                                           @RequestParam(name = "contenttypeid", required = false) String contenttypeid,
+                                           @RequestParam(name = "pageno", required = false) String pageNo,
+                                           @RequestParam(name = "cat1", required = false) String cat1) throws URISyntaxException{
 
-        return ResponseEntity.ok().body(tripService.recommendTrip(areaCode, sigunguCode, pageNo));
+        return ResponseEntity.ok().body(tripService.recommendTrip(areaCode, sigunguCode, contenttypeid, pageNo, cat1));
     }
 
 }
