6fb15effbe2a05e847eb6419b2d20680d3e6bc8c
박기진 <dugudada01@naver.com>
2024-06-24 23:16:27 +0900

    memberdetail에서 mid값 추출해서 리뷰 저장

--- src/main/java/Iniro/kTrip/controller/ReviewController.java
+++ src/main/java/Iniro/kTrip/controller/ReviewController.java
@@ -1,8 +1,10 @@
 package Iniro.kTrip.controller;
 
+import Iniro.kTrip.domain.Member;
 import Iniro.kTrip.domain.ReviewData;
 import Iniro.kTrip.dto.MemberDetails;
 import Iniro.kTrip.dto.ReviewDto;
+import Iniro.kTrip.repository.MemberRepository;
 import Iniro.kTrip.service.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,7 +19,8 @@ import java.util.List;
 public class ReviewController {
     @Autowired
     private ReviewService reviewService;
-
+    @Autowired
+    private MemberRepository memberRepository;
     @GetMapping()
     public ResponseEntity<?> getReviewListJson(@AuthenticationPrincipal MemberDetails memberDetails, @RequestParam("ctypeid") int ctypeid, @RequestParam("cid") int cid) {
         if (memberDetails != null) {
@@ -31,7 +34,9 @@ public class ReviewController {
     @PostMapping("/write")
     public ResponseEntity<?> createReview(@AuthenticationPrincipal MemberDetails memberDetails, @RequestBody ReviewData reviewData) {
         if (memberDetails != null) {
-            reviewService.registerReview(reviewService.createReview(reviewData));
+            Member member= memberRepository.findById(memberDetails.getId());
+            System.out.println(member.getMid());
+            reviewService.registerReview(reviewService.createReview(member.getMid(),reviewData));
             return ResponseEntity.ok("리뷰가 성공적으로 작성되었습니다.");
         } else {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("사용자에게 접근 권한이 없습니다");
