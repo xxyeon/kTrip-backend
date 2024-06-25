40be5cec798f1a0e178fc322bd254c18f842670e
박기진 <dugudada01@naver.com>
2024-06-25 18:50:17 +0900

    토큰 연동

--- src/main/java/Iniro/kTrip/controller/FavoriteController.java
+++ src/main/java/Iniro/kTrip/controller/FavoriteController.java
@@ -1,7 +1,11 @@
 package Iniro.kTrip.controller;
 
+import Iniro.kTrip.domain.Member;
+import Iniro.kTrip.dto.MemberDetails;
+import Iniro.kTrip.repository.MemberRepository;
 import Iniro.kTrip.service.FavoriteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -10,16 +14,24 @@ import org.springframework.web.bind.annotation.*;
 public class FavoriteController {
     @Autowired
     private FavoriteService favoriteService;
+    @Autowired
+    private MemberRepository memberRepository;
 
     @PostMapping("/toggle")
-    public void toggleFavoriteSpot(@RequestParam(name = "cid", required = true) String cid,
-                                @RequestParam(name = "mid", required = true) int mid,
+    public void toggleFavoriteSpot(
+                                @AuthenticationPrincipal MemberDetails memberDetails,
+                                @RequestParam(name = "cid", required = true) String cid,
                                 @RequestParam(name = "toggle", required = true) int toggle) throws IllegalAccessException {
-        if(toggle == 0){
-            favoriteService.deleteFavoriteSpot(cid, mid);
+        Member member=memberRepository.findById(memberDetails.getId());
+        if(member!=null)
+        {
+            if(toggle == 0){
+                favoriteService.deleteFavoriteSpot(cid, member.getMid());
+            }
+            else if(toggle == 1){
+                favoriteService.addFavoriteSpot(cid, member.getMid());
+            }
         }
-        else if(toggle == 1){
-            favoriteService.addFavoriteSpot(cid, mid);
-        }
+
     }
 }
