d58848fae293b4a97dd697944c0fa1812d2c8665
박진영 <am030813@naver.com>
2024-06-28 18:02:16 +0900

    ADD: Header에 Authorization 정보 노출

--- src/main/java/Iniro/kTrip/config/SecurityConfig.java
+++ src/main/java/Iniro/kTrip/config/SecurityConfig.java
@@ -63,7 +63,7 @@ public class SecurityConfig{
         http.csrf(csrf -> csrf.disable())
                 .cors(cors -> cors.configurationSource(corsConfigurationSource())) // CORS 설정 추가
                 .authorizeHttpRequests(requests -> requests
-                        .requestMatchers("/", "/oauth2/**", "/signUp", "/signIn", "/trip", "/trip/*", "/trip/**", "/reviews").permitAll()
+                        .requestMatchers("/", "/oauth2/**", "/signUp", "/signIn", "/trip", "/trip/*", "/trip/**", "/reviews", "/mypage", "/login").permitAll()
                         .requestMatchers("/public/**").permitAll()
                         .anyRequest().authenticated()
                 )
@@ -104,6 +104,7 @@ public class SecurityConfig{
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+        configuration.setExposedHeaders(Arrays.asList("Authorization"));
         configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
