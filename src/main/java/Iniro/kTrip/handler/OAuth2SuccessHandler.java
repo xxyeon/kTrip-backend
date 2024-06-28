29bf00b427e89bb15ccd70d7f8fd74fac5ccfc4b
박기진 <dugudada01@naver.com>
2024-06-28 14:29:27 +0900

    FIX: access->Authorization로 변경 및 Bearer추가

--- src/main/java/Iniro/kTrip/handler/OAuth2SuccessHandler.java
+++ src/main/java/Iniro/kTrip/handler/OAuth2SuccessHandler.java
@@ -6,6 +6,7 @@ import Iniro.kTrip.dto.MemberDto;
 import Iniro.kTrip.jwt.JWTUtil;
 import Iniro.kTrip.repository.MemberRepository;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +17,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
+import java.util.Date;
 
 @Component
 @RequiredArgsConstructor
@@ -35,10 +37,12 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         String userId = oAuth2User.getName();
         Member member =  memberRepository.findById(userId);
         //MemberDto memberDto = new MemberDto(member.getMember_id(), member.getId(), member.getPassword(), member.getEmail(), member.getNickname(), member.getName());
-        String token = jwtProvider.createAccessToken(member, 100000);
+        String token = jwtProvider.createAccessToken("Authorization",member, 100000);
+        String refresh =jwtProvider.createAccessToken("refresh",member,86400000);
 
-
-
+        response.addHeader("Authorization", token);
+        response.addCookie(createCookie("refresh", refresh));
+        addRefreshEntity(userId, refresh, 86400000);
         targetUrl = UriComponentsBuilder.fromUriString(redirectUri)
                 .queryParam("token", token)
                 .build().toUriString();
@@ -46,5 +50,22 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         clearAuthenticationAttributes(request);
         getRedirectStrategy().sendRedirect(request, response, targetUrl);
         //response.sendRedirect("http://localhost:8080/auth/oauth-response/"+token+"/3600");
+
+
+    }
+    private void addRefreshEntity(String id, String refresh, int expiredMs) {
+        Date date = new Date(System.currentTimeMillis() + expiredMs);
+        Member member = memberRepository.findById(id);
+        if (member != null) {
+            member.setRefreshToken(refresh);
+            member.setExpiration(date.toString());
+            memberRepository.save(member);
+        }
+    }
+    private Cookie createCookie(String key, String value) {
+        Cookie cookie = new Cookie(key, value);
+        cookie.setMaxAge(24 * 60 * 60);
+        cookie.setHttpOnly(true);
+        return cookie;
     }
 }
